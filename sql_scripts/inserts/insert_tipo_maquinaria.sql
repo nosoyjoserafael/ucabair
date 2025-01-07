a84cb6c9e2fca4ed4a6a7dc1758a29d87577b290
koprolito <130481373+koprolito@users.noreply.github.com>
2025-01-07 19:35:00 -0400

    archivos de insert y secuencias para tablas que no se crean por defecto con ellas

---
+++ sql_scripts/inserts/insert_tipo_maquinaria.sql
@@ -0,0 +1,10 @@
+insert into tipo_maquinaria (tipo_maq_nombre) values ('Maquinaria para mantenimiento');
+insert into tipo_maquinaria (tipo_maq_nombre) values ('Tornos');
+insert into tipo_maquinaria (tipo_maq_nombre) values ('Máquinas de corte por láser');
+insert into tipo_maquinaria (tipo_maq_nombre) values ('Robots industriales');
+insert into tipo_maquinaria (tipo_maq_nombre) values ('Autoclaves');
+insert into tipo_maquinaria (tipo_maq_nombre) values ('Grúas');
+insert into tipo_maquinaria (tipo_maq_nombre) values ('Plataformas elevadoras');
+insert into tipo_maquinaria (tipo_maq_nombre) values ('Taladros');
+insert into tipo_maquinaria (tipo_maq_nombre) values ('Equipos de inspección');
+insert into tipo_maquinaria (tipo_maq_nombre) values ('Pistolas de remachado');
