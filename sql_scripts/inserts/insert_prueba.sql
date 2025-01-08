a6f0e5c1e415970493a00d4c5e86454a90ab18d5
koprolito <130481373+koprolito@users.noreply.github.com>
2025-01-07 21:27:57 -0400

    insert de prueba y tipo_prueba

---
+++ sql_scripts/inserts/insert_prueba.sql
@@ -0,0 +1,11 @@
+--se debe insertar despues de tipo_prueba para que no hayan conflictos
+insert into prueba ("fk_tipoPrueba") values (1);
+insert into prueba ("fk_tipoPrueba") values (2);
+insert into prueba ("fk_tipoPrueba") values (3);
+insert into prueba ("fk_tipoPrueba") values (4);
+insert into prueba ("fk_tipoPrueba") values (5);
+insert into prueba ("fk_tipoPrueba") values (6);
+insert into prueba ("fk_tipoPrueba") values (7);
+insert into prueba ("fk_tipoPrueba") values (8);
+insert into prueba ("fk_tipoPrueba") values (9);
+insert into prueba ("fk_tipoPrueba") values (10);
---
+++ sql_scripts/inserts/insert_tipo_prueba.sql
@@ -0,0 +1,10 @@
+insert into tipo_prueba (tprueba_nombre, tprueba_duracion_estim) values ('sistemas de aviónica', 42);
+insert into tipo_prueba (tprueba_nombre, tprueba_duracion_estim) values ('fatiga', 22);
+insert into tipo_prueba (tprueba_nombre, tprueba_duracion_estim) values ('vibración', 13);
+insert into tipo_prueba (tprueba_nombre, tprueba_duracion_estim) values ('vibración', 10);
+insert into tipo_prueba (tprueba_nombre, tprueba_duracion_estim) values ('fatiga', 20);
+insert into tipo_prueba (tprueba_nombre, tprueba_duracion_estim) values ('ruido', 4);
+insert into tipo_prueba (tprueba_nombre, tprueba_duracion_estim) values ('sistemas', 12);
+insert into tipo_prueba (tprueba_nombre, tprueba_duracion_estim) values ('rendimiento', 23);
+insert into tipo_prueba (tprueba_nombre, tprueba_duracion_estim) values ('sistemas de aviónica', 7);
+insert into tipo_prueba (tprueba_nombre, tprueba_duracion_estim) values ('vibración', 13);
