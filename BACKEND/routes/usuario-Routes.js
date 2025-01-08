c82d23252f94b5b2dafc0f4f68532a1fe4255ebb
koprolito <130481373+koprolito@users.noreply.github.com>
2025-01-08 14:18:17 -0400

    chau back

--- BACKEND/routes/usuario-Routes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-const { createData } = require('../controllers/usuario-Controller');
-
-router.post('/', createData); // Crear datos
-
-module.exports = router;
