7719604719bb25d358458330808f982265420604
koprolito <130481373+koprolito@users.noreply.github.com>
2025-01-08 15:56:24 -0400

    modificaciones en el navbar para mejorar dinamismo y la interaccion entre roles de usuario

---
+++ FRONTEND/src/utils/navbar.js
@@ -0,0 +1,70 @@
+document.addEventListener('DOMContentLoaded', () => {
+    //const token = localStorage.getItem('token');
+    const token = 'empleado'; //solo para pruebas
+
+    localStorage.setItem('token', token); //solo para pruebas
+
+    const storedToken = localStorage.getItem('token'); //solo para pruebas
+
+    const overlay = document.getElementById('generic-overlay');
+    const overlayMatrix = document.querySelector('.overlay-matrix');
+
+    overlay.addEventListener('click', function(event) {
+        if (event.target === overlay) {
+            overlay.classList.remove('visible');
+            adjustIframeSize(false);
+        }
+    });
+
+    function addURLsEmployee() {        
+        //Aqui se agregan los urls al 
+        // overLay segun sea necesario        
+        overlayMatrix.innerHTML = '';
+        const urls = ['../pages/crud-proveedor.html', '../pages/crud-empleado.html','../pages/crud-modelo.html'];
+        urls.forEach(url => {
+            const spanUrl = document.createElement('span');
+            const urlElement = document.createElement('a');    
+            urlElement.textContent = 'Proveedores';
+            urlElement.href = url;        
+            spanUrl.appendChild(urlElement); 
+            spanUrl.addEventListener('click', () => {window.location.href = url;});       
+            overlayMatrix.appendChild(spanUrl);
+        });
+    }
+
+    function adjustIframeSize(expand) {
+        const iframe = window.frameElement;
+        if (iframe) {
+            if (expand) {
+                iframe.style.position = 'fixed';
+                iframe.style.top = '0';
+                iframe.style.left = '0';
+                iframe.style.width = '100%';
+                iframe.style.height = '100%';
+                iframe.style.zIndex = '1000';
+            } else {
+                iframe.style.position = '';
+                iframe.style.top = '';
+                iframe.style.left = '';
+                iframe.style.width = '100%';
+                iframe.style.height = 'auto';
+                iframe.style.zIndex = '';
+            }
+        }
+    }
+
+    // Decodificar el token para obtener la información del usuario (aun no está integrado)
+    //const payload = JSON.parse(atob(storedToken.split('.')[1]));
+
+    // Verificar si el usuario tiene el rol necesario
+    if(storedToken.includes("empleado")){
+        document.getElementById('gestion-a').addEventListener('click', () => {
+            overlay.classList.add('visible');
+            adjustIframeSize(true);
+            addURLsEmployee();
+        });        
+    }
+    else{
+        document.getElementById('overlay').remove();
+    }
+});
