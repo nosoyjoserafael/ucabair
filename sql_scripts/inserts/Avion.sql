7adbd2ed8273f232f2c11079136ebc01cf53ca05
koprolito <130481373+koprolito@users.noreply.github.com>
2025-01-09 13:03:14 -0400

    modificaciones a archivos de insert para corregir funcionalidad

--- sql_scripts/inserts/Avion.sql
+++
@@ -1,6 +0,0 @@
-CREATE SEQUENCE av_cod_seq START WITH 1 INCREMENT BY 1;
-
-insert into public."avion" ("avion_cod", "fk_modelo") values (nextval('av_cod_seq'), 1);
-insert into public."avion" ("avion_cod", "fk_modelo") values (nextval('av_cod_seq'), 2);
-insert into public."avion" ("avion_cod", "fk_modelo") values (nextval('av_cod_seq'), 3);
-insert into public."avion" ("avion_cod", "fk_modelo") values (nextval('av_cod_seq'), 9);
---
+++ sql_scripts/inserts/insert_avion.sql
@@ -0,0 +1,4 @@
+insert into public.avion (fk_modelo) values (1);
+insert into public.avion (fk_modelo) values (2);
+insert into public.avion (fk_modelo) values (3);
+insert into public.avion (fk_modelo) values (9);
