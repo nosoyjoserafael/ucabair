5d321a29018e059ed40ab3e00f9a4f4b0ac8b9eb
koprolito <130481373+koprolito@users.noreply.github.com>
2025-01-12 21:51:36 -0400

    modificacion del insert definitivo y modificacion de la funcionalidad del trigger

--- sql_scripts/triggers/trigger_compra_material.sql
+++ sql_scripts/triggers/trigger_compra_material.sql
@@ -1,23 +1,18 @@
-CREATE OR REPLACE FUNCTION insertar_compra(
-    p_nombre_material VARCHAR,
+CREATE OR REPLACE FUNCTION compra_materiales_proveedor(
+    p_tipo_material_cod INTEGER,
     p_cantidad INTEGER
 ) RETURNS INTEGER AS $$
 DECLARE
     v_costo_unitario NUMERIC;
-    v_id_tipo_mat INTEGER;
     v_id_compra INTEGER;
     v_id_det_compra INTEGER;
     v_monto_total NUMERIC;
-BEGIN
-    -- Obtener el ID del tipo de material
-    SELECT tipo_mat_cod INTO v_id_tipo_mat
-    FROM tipo_material
-    WHERE tipo_mat_nombre = p_nombre_material;
-
+    v_prove_cod INTEGER;
+BEGIN    
     -- Obtener el costo unitario del material
     SELECT promat_costo INTO v_costo_unitario
     FROM pro_mat
-    WHERE fk_tmat = v_id_tipo_mat;
+    WHERE fk_tmat = p_tipo_material_cod;
 
     -- Calcular el monto total
     v_monto_total := p_cantidad * v_costo_unitario;
@@ -26,24 +21,35 @@ BEGIN
     INSERT INTO compra (compr_preciotot, fk_proveedor)
     VALUES (v_monto_total, (SELECT prove_cod
 							FROM proveedor
+                            WHERE prove_cod in (SELECT fk_proveedor
+                                                FROM pro_mat
+                                                WHERE fk_tmat = p_tipo_material_cod AND
+                                                fk_proveedor = prove_cod AND
+                                                promat_cantidad >= p_cantidad)
 							ORDER BY random()
 							LIMIT 1))
-    RETURNING compr_cod INTO v_id_compra;
+    RETURNING compr_cod, fk_proveedor INTO v_id_compra, v_prove_cod;
 
-    IF (SELECT count(*) FROM material WHERE "fk_TMaterial" = v_id_tipo_mat AND fk_sede = 1) = 0 THEN
+    -- Actualizar la cantidad de material en el proveedor
+    UPDATE pro_mat
+    SET promat_cantidad = promat_cantidad - p_cantidad
+    WHERE fk_tmat = p_tipo_material_cod
+    AND fk_proveedor = v_prove_cod;
+
+    IF (SELECT count(*) FROM material WHERE "fk_TMaterial" = p_tipo_material_cod AND fk_sede = 1) = 0 THEN
         -- Insertar el material para la sede de La Guaira
         INSERT INTO material (mat_cantidad, mat_fecha_in, "fk_TMaterial", fk_sede)
-        VALUES (p_cantidad, current_date, v_id_tipo_mat, 1);
+        VALUES (p_cantidad, current_date, p_tipo_material_cod, 1);
     ELSE
         -- Actualizar el stock del material
         UPDATE material 
         SET mat_cantidad = mat_cantidad + p_cantidad
-        WHERE "fk_TMaterial" = v_id_tipo_mat;
+        WHERE "fk_TMaterial" = p_tipo_material_cod;
     END IF;
 
     -- Insertar el detalle de la compra
     INSERT INTO detalle_compra (detcomp_fecha_compra, detcomp_cantidad, fk_compra, fk_material)
-    VALUES (current_date, p_cantidad, v_id_compra, v_id_tipo_mat)
+    VALUES (current_date, p_cantidad, v_id_compra, p_tipo_material_cod)
     RETURNING detcomp_cod INTO v_id_det_compra;
 
     RETURN v_id_det_compra;
@@ -57,14 +63,14 @@ $$ LANGUAGE plpgsql;
 
 CREATE OR REPLACE FUNCTION verificar_stock() RETURNS trigger AS $$
 DECLARE
-    v_nombre_material VARCHAR;
+    v_tipo_material_cod INTEGER;
 BEGIN
   IF NEW.mat_cantidad < 200 THEN
-    SELECT tipo_mat_nombre INTO v_nombre_material
+    SELECT tipo_mat_cod INTO v_tipo_material_cod
     FROM tipo_material
     WHERE tipo_mat_cod = NEW."fk_TMaterial";
 
-    PERFORM insertar_compra(v_nombre_material, 200);
+    PERFORM compra_materiales_proveedor(v_tipo_material_cod, 200);
   END IF;
   RETURN NEW;
 END;
