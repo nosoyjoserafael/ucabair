6cfe536c048daff83a964f9295f05c456c6afa49
Matias Silveira <jmsilveira.22@est.ucab.edu.ve>
2025-01-14 11:43:33 -0400

    correcciones en las funciones para que reciba el codigo del personal

--- sql_scripts/functions/function_calcular_horas_extra.sql
+++ sql_scripts/functions/function_calcular_horas_extra.sql
@@ -1,13 +1,11 @@
 CREATE OR REPLACE FUNCTION calcular_horas_extra(
-    p_user_nombre VARCHAR
+    p_per_cod INTEGER
 )
 RETURNS TABLE(
     per_cod INTEGER,
-    per_nombre_usuario VARCHAR,
     horas_extra INTEGER
 ) AS $$
 DECLARE
-    v_per_cod INTEGER;
     v_hora_entrada TIME WITHOUT TIME ZONE;
     v_hora_salida TIME WITHOUT TIME ZONE;
     v_hora_entrada_horario TIME WITHOUT TIME ZONE;
@@ -16,33 +14,37 @@ DECLARE
     v_total_horas_extra INTEGER := 0;
 BEGIN
     -- Verificar si el usuario existe
-    IF p_user_nombre IS NOT NULL AND p_user_nombre NOT IN (SELECT user_nombre FROM usuario) THEN
-        RAISE EXCEPTION 'Error, el nombre de usuario no existe';
+    IF p_per_cod IS NOT NULL AND p_per_cod NOT IN (SELECT "Per_cod" FROM "Personal") THEN
+        RAISE EXCEPTION 'Error, el codigo de personal no existe';
     END IF;
 
     -- Obtener las horas de entrada y salida del horario regular del empleado
-    SELECT hor.hora_entrada, hor.hora_salida, usr.fk_personal
-    INTO v_hora_entrada_horario, v_hora_salida_horario, v_per_cod
+    SELECT hor.hora_entrada, hor.hora_salida
+    INTO v_hora_entrada_horario, v_hora_salida_horario
     FROM horario hor
     JOIN asignacion asig ON hor.hora_cod = asig.fk_horario
-    JOIN usuario usr ON usr.fk_personal = asig.fk_personal
-    WHERE usr.user_nombre = p_user_nombre;
+    JOIN "Personal" per ON per."Per_cod" = asig.fk_personal
+    WHERE per."Per_cod" = p_per_cod;
 
     -- Calcular las horas extra para todas las asistencias del usuario
     FOR v_hora_entrada, v_hora_salida IN
         SELECT asis.asis_hora_entrada, asis.asis_hora_salida
         FROM asistencia asis
         JOIN asignacion asig ON asis.fk_asignacion = asig.asig_cod
-        JOIN usuario usr ON asig.fk_personal = usr.fk_personal
-        WHERE usr.user_nombre = p_user_nombre
+        JOIN "Personal" per ON asig.fk_personal = per."Per_cod"
+        WHERE per."Per_cod" = p_per_cod
     LOOP
         IF v_hora_salida > v_hora_salida_horario THEN
             v_horas_extra := v_hora_salida - v_hora_salida_horario;
             v_total_horas_extra := v_total_horas_extra + FLOOR(EXTRACT(EPOCH FROM v_horas_extra) / 3600);
         END IF;
+        IF v_hora_entrada < v_hora_entrada_horario THEN
+            v_horas_extra := v_hora_entrada_horario - v_hora_entrada;
+            v_total_horas_extra := v_total_horas_extra + FLOOR(EXTRACT(EPOCH FROM v_horas_extra) / 3600);
+        END IF;
     END LOOP;
 
     RETURN QUERY
-    SELECT v_per_cod, p_user_nombre, v_total_horas_extra;
+    SELECT p_per_cod, v_total_horas_extra;
 END;
 $$ LANGUAGE plpgsql;
--- sql_scripts/functions/function_calcular_nomina.sql
+++ sql_scripts/functions/function_calcular_nomina.sql
@@ -1,5 +1,5 @@
 CREATE OR REPLACE FUNCTION calcular_nomina(
-    p_user_nombre VARCHAR,
+    p_per_cod INTEGER,
     p_fecha_inicio DATE,
     p_fecha_fin DATE
 )
@@ -12,7 +12,6 @@ RETURNS TABLE(
     horas_extra_realizadas INTEGER
 ) AS $$
 DECLARE
-    v_per_cod INTEGER;
     v_sueldo_base NUMERIC;
     v_horas_extra INTEGER;
     v_sueldo_hora_extra NUMERIC;
@@ -21,29 +20,23 @@ DECLARE
     v_total_nomina NUMERIC;
 BEGIN
     -- Verificar si el usuario existe
-    IF p_user_nombre IS NOT NULL AND p_user_nombre NOT IN (SELECT user_nombre FROM usuario) THEN
-        RAISE EXCEPTION 'Error, el nombre de usuario no existe';
+    IF p_per_cod IS NOT NULL AND p_per_cod NOT IN (SELECT "Per_cod" FROM "Persona") THEN
+        RAISE EXCEPTION 'Error, el codigo del personal no existe';
     END IF;
 
-    SELECT horas_extra from calcular_horas_extra(p_user_nombre) INTO v_horas_extra;
-
-    -- Obtener el codigo del personal
-    SELECT fk_personal
-    INTO v_per_cod
-    FROM usuario
-    WHERE user_nombre = p_user_nombre;
+    SELECT horas_extra from calcular_horas_extra(p_per_cod) INTO v_horas_extra;
 
     -- Obtener el sueldo base y el sueldo por hora extra
     select pc.per_car_sueldo_base, car.car_sueldo_hora_extra
     INTO v_sueldo_base, v_sueldo_hora_extra
     from per_car pc, cargo car
-    WHERE pc.fk_personal = v_per_cod and pc.fk_cargo = car.car_cod;
+    WHERE pc.fk_personal = p_per_cod and pc.fk_cargo = car.car_cod;
 
     -- obtener el codigo de su asignacion
     SELECT asig_cod
     INTO v_asig_cod
     FROM asignacion
-    WHERE fk_personal = v_per_cod;
+    WHERE fk_personal = p_per_cod;
 
     -- Obtener la cantidad de veces que ha asistido
     SELECT COUNT(*)
@@ -55,7 +48,7 @@ BEGIN
     -- Calcular el total de la nomina
     total_nomina := v_sueldo_base*v_asistencias + (v_sueldo_hora_extra * v_horas_extra)*v_asistencias;
 
-    RETURN QUERY SELECT v_per_cod, total_nomina, v_asistencias, v_sueldo_base, v_sueldo_hora_extra, v_horas_extra;
+    RETURN QUERY SELECT p_per_cod, total_nomina, v_asistencias, v_sueldo_base, v_sueldo_hora_extra, v_horas_extra;
 
 END;
 $$ LANGUAGE plpgsql;
