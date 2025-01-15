d3bf558ef2dc5ef510d5aa124a6537d2d741f163
koprolito <130481373+koprolito@users.noreply.github.com>
2025-01-14 22:19:02 -0400

    funciones de crud de equipo

--- sql_scripts/inserts/insert_trabajo_equipo.sql
+++ sql_scripts/inserts/insert_trabajo_equipo.sql
@@ -1,11 +1,12 @@
 --insertar despues de equipo y construccion
-INSERT INTO trabajo_equipo (te_tiempo_estim, te_nombre_trabajo, fk_equipo) VALUES ('2026-01-01',  'Montaje de alas', 1);
-INSERT INTO trabajo_equipo (te_tiempo_estim, te_nombre_trabajo, fk_equipo) VALUES ('2020-01-01',  'Instalación de motores', 2);
-INSERT INTO trabajo_equipo (te_tiempo_estim, te_nombre_trabajo, fk_equipo) VALUES ('2020-01-01',  'Montaje de fuselaje', 3);
-INSERT INTO trabajo_equipo (te_tiempo_estim, te_nombre_trabajo, fk_equipo) VALUES ('2020-01-01',  'Instalación de sistemas eléctricos', 4);
-INSERT INTO trabajo_equipo (te_tiempo_estim, te_nombre_trabajo, fk_equipo) VALUES ('2020-01-01',  'Pintura y acabado', 5);
-INSERT INTO trabajo_equipo (te_tiempo_estim, te_nombre_trabajo, fk_equipo) VALUES ('2020-01-01',  'Pruebas de vuelo', 6);
-INSERT INTO trabajo_equipo (te_tiempo_estim, te_nombre_trabajo, fk_equipo) VALUES ('2020-01-01',  'Montaje de tren de aterrizaje', 7);
-INSERT INTO trabajo_equipo (te_tiempo_estim, te_nombre_trabajo, fk_equipo) VALUES ('2020-01-01',  'Instalación de sistemas hidráulicos', 8);
-INSERT INTO trabajo_equipo (te_tiempo_estim, te_nombre_trabajo, fk_equipo) VALUES ('2020-01-01',  'Montaje de cabina', 9);
-INSERT INTO trabajo_equipo (te_tiempo_estim, te_nombre_trabajo, fk_equipo) VALUES ('2020-01-01',  'Control de calidad', 10);
+INSERT INTO trabajo_equipo (te_tiempo_estim, te_nombre_trabajo, te_tiempo_real, fk_equipo) VALUES ('2026-01-01',  'Montaje de alas', '2025-01-15',1);
+INSERT INTO trabajo_equipo (te_tiempo_estim, te_nombre_trabajo, te_tiempo_real, fk_equipo) VALUES ('2026-01-01',  'Instalación de motores', '2025-01-15',1);
+INSERT INTO trabajo_equipo (te_tiempo_estim, te_nombre_trabajo, te_tiempo_real, fk_equipo) VALUES ('2026-01-01',  'Instalación de motores', '2025-01-15',2);
+INSERT INTO trabajo_equipo (te_tiempo_estim, te_nombre_trabajo, te_tiempo_real, fk_equipo) VALUES ('2026-01-01',  'Montaje de fuselaje', '2025-01-15',3);
+INSERT INTO trabajo_equipo (te_tiempo_estim, te_nombre_trabajo, te_tiempo_real, fk_equipo) VALUES ('2026-01-01',  'Instalación de sistemas eléctricos', '2025-01-15',4);
+INSERT INTO trabajo_equipo (te_tiempo_estim, te_nombre_trabajo, te_tiempo_real, fk_equipo) VALUES ('2026-01-01',  'Pintura y acabado', '2025-01-15',5);
+INSERT INTO trabajo_equipo (te_tiempo_estim, te_nombre_trabajo, te_tiempo_real, fk_equipo) VALUES ('2026-01-01',  'Pruebas de vuelo', '2025-01-15',6);
+INSERT INTO trabajo_equipo (te_tiempo_estim, te_nombre_trabajo, te_tiempo_real, fk_equipo) VALUES ('2026-01-01',  'Montaje de tren de aterrizaje', '2025-01-15',7);
+INSERT INTO trabajo_equipo (te_tiempo_estim, te_nombre_trabajo, te_tiempo_real, fk_equipo) VALUES ('2026-01-01',  'Instalación de sistemas hidráulicos', '2025-01-15',8);
+INSERT INTO trabajo_equipo (te_tiempo_estim, te_nombre_trabajo, te_tiempo_real, fk_equipo) VALUES ('2026-01-01',  'Montaje de cabina', '2025-01-15',9);
+INSERT INTO trabajo_equipo (te_tiempo_estim, te_nombre_trabajo, te_tiempo_real, fk_equipo) VALUES ('2026-01-01',  'Control de calidad', '2025-01-15',10);
