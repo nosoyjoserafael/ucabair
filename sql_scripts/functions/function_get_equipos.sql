d47ef3c439891771796febe52f31d4549b5106fb
koprolito <130481373+koprolito@users.noreply.github.com>
2025-01-14 22:24:03 -0400

    modificacion en get_equipos()

--- sql_scripts/functions/function_get_equipos.sql
+++ sql_scripts/functions/function_get_equipos.sql
@@ -1,13 +1,14 @@
 CREATE OR REPLACE FUNCTION get_equipos()
 RETURNS TABLE(
     equi_cod INTEGER,
+    te_cod INTEGER,
     nombre_trabajo VARCHAR,
     tiempo_estim DATE,
     tiempo_real DATE
 ) AS $$
 BEGIN
     RETURN QUERY
-    SELECT eq."Equi_cod", te.te_nombre_trabajo, te.te_tiempo_estim, te.te_tiempo_real
+    SELECT eq."Equi_cod", te.te_cod, te.te_nombre_trabajo, te.te_tiempo_estim, te.te_tiempo_real
     from "Equipo" eq JOIN trabajo_equipo te ON eq."Equi_cod" = te.fk_equipo;
 END;
 $$ LANGUAGE plpgsql;
