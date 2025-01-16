1902583eba03fb4a299f5bb5f7398b0e72eceb1a
koprolito <130481373+koprolito@users.noreply.github.com>
2025-01-16 19:49:29 -0400

    inventario_detallado_por_mes()

---
+++ sql_scripts/functions/function_inventario_detallado_por_mes.sql
@@ -0,0 +1,21 @@
+CREATE OR REPLACE FUNCTION inventario_detallado_por_mes()
+RETURNS TABLE(
+    sede_inventario VARCHAR,
+    mes_inventario TIMESTAMP WITHOUT TIME ZONE,
+    material_nombre VARCHAR,
+    material_cantidad_inventario INTEGER
+) AS $$
+BEGIN
+    RETURN QUERY
+    SELECT 
+        s.sed_nombre, mat.mat_fecha_in, tmat.tipo_mat_nombre, mat.mat_cantidad
+    FROM material mat
+        JOIN tipo_material tmat ON mat."fk_TMaterial" = tmat.tipo_mat_cod
+        JOIN sede s ON mat.fk_sede = s.sed_cod
+    GROUP BY
+        s.sed_nombre, mat.mat_fecha_in, tmat.tipo_mat_nombre, mat.mat_cantidad
+    ORDER BY
+        1, 2 ASC
+    ;
+END;
+$$ LANGUAGE plpgsql;
