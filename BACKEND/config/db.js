74afb369631440f93b0f08aaa9b88f86adaffc99
koprolito <130481373+koprolito@users.noreply.github.com>
2025-01-19 13:06:38 -0400

    modificaciones varias

--- BACKEND/config/db.js
+++ BACKEND/config/db.js
@@ -1,11 +1,19 @@
+const path = require('path');
+require('dotenv').config({ path: path.resolve(__dirname, '../../.env') });
 const { Pool } = require('pg');
 
+console.log('DB_USER:', process.env.DB_USER);
+console.log('DB_HOST:', process.env.DB_HOST);
+console.log('DB_NAME:', process.env.DB_NAME);
+console.log('DB_PASSWORD:', process.env.DB_PASSWORD);
+console.log('DB_PORT:', process.env.DB_PORT);
+
 const pool = new Pool({
-  user: process.env.DB_USER || 'grupo_rsm',
-  host: process.env.DB_HOST || 'labs-dbservices01.ucab.edu.ve',
-  database: process.env.DB_NAME || 'amarquez',
-  password: process.env.DB_PASSWORD || '123456',
-  port: process.env.DB_PORT || 5432,
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT
 });
 
 module.exports = pool;
